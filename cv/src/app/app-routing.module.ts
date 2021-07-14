cd32573cb48b95e01c54da2209b29678fbb10424
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-14 16:44:25 +0300

    add employee

--- cv/src/app/app-routing.module.ts
+++ cv/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }];
+const routes: Routes = [{ path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }, { path: 'employee', loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
