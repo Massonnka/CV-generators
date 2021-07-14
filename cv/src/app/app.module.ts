1d035c10651eb751ddbba7d9843aa0feb6b0fb82
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-14 16:54:04 +0300

    add global-header

--- cv/src/app/app.module.ts
+++ cv/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './pages/auth/auth.module';
+import { GlobalHeaderComponent } from './global-header/global-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalHeaderComponent
   ],
   imports: [
     BrowserModule,
