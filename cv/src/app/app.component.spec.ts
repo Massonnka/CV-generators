3b661ed8d6684559bdca82e4d2e4e7ebec3cb652
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-14 16:33:16 +0300

    add project module

--- cv/src/app/app.component.spec.ts
+++ cv/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'cv'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cv');
+    expect(app).toEqual('cv');
   });
 
   it('should render title', () => {
