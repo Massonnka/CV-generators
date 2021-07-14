44fc04c93b8a90efdaadd746cd1b6ff3321df5f7
Nikita Shcherbich <shcherbich.n@mail.ru>
2021-07-14 16:50:45 +0300

    change import

--- cv/src/app/app-routing.module.ts
+++ cv/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }];
+const routes: Routes = [{ path: 'project', loadChildren: () => import('./pages/project/project.module').then(m => m.ProjectModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
