32f04dd64e199cb76909f465b38c238be017cdf8
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-15 15:43:51 +0300

    add auth-logo component

---
+++ cv/src/app/shared/controls/auth/frame-logo/components/auth-logo/auth-logo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthLogoComponent } from './auth-logo.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthLogoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthLogoModule { }
