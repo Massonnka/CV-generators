2270bee54192cb1b16973d8cbf1c6bcd719cc728
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-15 12:06:34 +0300

    add auth-button-global component

--- cv/src/app/app.module.ts
+++ cv/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './modules/auth/auth.module';
+import { ButtonGlobalComponent } from './button-global/button-global.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonGlobalComponent
   ],
   imports: [
     BrowserModule,
---
+++ cv/src/app/shared/controls/auth/button-global/button-global.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-global',
+  templateUrl: './button-global.component.html',
+  styleUrls: ['./button-global.component.css']
+})
+export class ButtonGlobalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
