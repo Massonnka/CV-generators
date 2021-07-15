563b2e8f856ceaf96a4c84fcb9220012ebfacaf5
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-15 12:16:36 +0300

    add form-auth-button component

---
+++ cv/src/app/shared/controls/auth/form-auth/components/form-auth-button/form-auth-button.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormAuthButtonComponent } from '../form-auth-button/form-auth-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormAuthButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormAuthButtonModule { }
