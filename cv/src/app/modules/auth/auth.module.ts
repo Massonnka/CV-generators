0163fe3276041d1c8f7357d3d4fc584a3231cae7
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-15 17:00:27 +0300

    add form-input

--- cv/src/app/modules/auth/auth.module.ts
+++ cv/src/app/modules/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { BgModule } from 'src/app/shared/controls/auth/bg/bg.module';
 import { ButtonGlobalModule } from 'src/app/shared/controls/auth/button-global/button-global.module';
 import { ButtonQuestionModule } from 'src/app/shared/controls/auth/button-question/button-question.module';
 import { FrameLogoModule } from 'src/app/shared/controls/auth/frame-logo/frame-logo.module';
+import { FormAuthModule } from 'src/app/shared/controls/auth/form-auth/form-auth.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FrameLogoModule } from 'src/app/shared/controls/auth/frame-logo/frame-l
     BgModule,
     ButtonGlobalModule,
     ButtonQuestionModule,
-    FrameLogoModule
+    FrameLogoModule,
+    FormAuthModule
   ],
   exports: [
     AuthComponent
