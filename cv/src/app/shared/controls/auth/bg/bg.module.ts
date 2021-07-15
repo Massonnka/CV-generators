e6a5927487ac281234b37b408d4361bf6ed6b25d
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-15 12:21:31 +0300

    add bg component

---
+++ cv/src/app/shared/controls/auth/bg/bg.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BgComponent } from './bg.component';
+
+
+
+@NgModule({
+  declarations: [
+    BgComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BgModule { }
