ba60745889b2bedaa51a7d648b938e4480aee58f
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-15 12:20:29 +0300

    add frame-logo component

---
+++ cv/src/app/shared/controls/auth/frame-logo/frame-logo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FrameLogoComponent } from './frame-logo.component';
+
+describe('FrameLogoComponent', () => {
+  let component: FrameLogoComponent;
+  let fixture: ComponentFixture<FrameLogoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FrameLogoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FrameLogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
