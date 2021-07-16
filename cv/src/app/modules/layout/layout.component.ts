afcb6c133a60338c814e556d810e150266f3c991
Nikita Shcherbich <shcherbich.n@mail.ru>
2021-07-16 13:42:42 +0300

    add switchable slider

--- cv/src/app/modules/layout/layout.component.ts
+++ cv/src/app/modules/layout/layout.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elRef: ElementRef) {}
 
-  ngOnInit(): void {
+  private siderVisible = true;
+
+  toggleSider(): void {
+    const sider = this.elRef.nativeElement.querySelector('.sider');
+    
+    if (this.siderVisible) {
+      sider.style.width = '10px'; 
+      sider.style.flex = '0 0 10px';
+      this.siderVisible = !this.siderVisible;
+    } else {
+      sider.style.width = '200px'; 
+      sider.style.flex = '0 0 200px';
+      this.siderVisible = !this.siderVisible;
+    }
   }
 
+  ngOnInit(): void {}
 }
