ace847ea0f5b0a10d3ea3ddc1e1a1943b942e2ec
Nikita Shcherbich <shcherbich.n@mail.ru>
2021-07-19 17:18:32 +0300

    add table

--- cv/src/app/modules/layout/pages/employee/employee.module.ts
+++ cv/src/app/modules/layout/pages/employee/employee.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { PrimaryButtonModule } from 'src/app/shared/controls/buttons/primary-button/primary-button.module';
+import { TableModule } from 'src/app/shared/controls/tables/table/table.module';
 import { GlobalHeaderModule } from '../../../global-header/global-header.module';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
@@ -16,6 +18,8 @@ import { EmployeeComponent } from './employee.component';
     NzBreadCrumbModule,
     NzLayoutModule,
     TranslateModule,
+    PrimaryButtonModule,
+    TableModule,
   ],
   exports: [EmployeeComponent],
 })
