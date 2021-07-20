48e8a18950666ecd688f6c8d36350dc2a23d0015
Nikita Shcherbich <shcherbich.n@mail.ru>
2021-07-20 17:29:33 +0300

    add dinamic profile name

--- cv/src/app/modules/layout/pages/employee/employee-profile/employee-profile.component.ts
+++ cv/src/app/modules/layout/pages/employee/employee-profile/employee-profile.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EMPLOYEES } from 'src/app/employees';
 
 @Component({
   selector: 'app-employee-profile',
   templateUrl: './employee-profile.component.html',
-  styleUrls: ['./employee-profile.component.scss']
+  styleUrls: ['./employee-profile.component.scss'],
 })
 export class EmployeeProfileComponent implements OnInit {
-  public cves:any = [
-    {name: 'cv 1'},
-    {name: 'cv 2'},
-    {name: 'cv 3'},
-    {name: 'cv 4'},
-    {name: 'cv 5'},
-  ]
+  public employees = EMPLOYEES;
 
-  constructor() { }
+  public cves: any = [
+    { name: 'cv 1' },
+    { name: 'cv 2' },
+    { name: 'cv 3' },
+    { name: 'cv 4' },
+    { name: 'cv 5' },
+  ];
 
-  ngOnInit(): void {
+  public currentUserId = 1;
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe((value) => {
+      this.currentUserId = value.user;      
+    });
   }
 
+  ngOnInit(): void {}
 }
