c451de8f21ef7eb8122284cf6597920a0f162f28
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-07-29 13:07:38 +0300

    add logout

--- cv/src/app/modules/layout/global-header/components/global-header.component.ts
+++ cv/src/app/modules/layout/global-header/components/global-header.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-global-header',
@@ -11,7 +13,10 @@ import { TranslateService } from '@ngx-translate/core';
 export class GlobalHeaderComponent implements OnInit {
   public languages = ['en', 'ru'];
 
-  constructor(private translateService: TranslateService, private store: Store) { }
+  constructor(private translateService:
+    TranslateService, private store: Store,
+    private router: Router,
+    public authService: AuthService) { }
   public currentLanguage = this.translateService.currentLang || 'en';
 
   switchLanguage(language: string): void {
@@ -20,4 +25,10 @@ export class GlobalHeaderComponent implements OnInit {
   }
 
   ngOnInit(): void { }
+
+  logout(event: Event) {
+    event.preventDefault();
+    this.authService.logout();
+    this.router.navigate(['auth/log-in']);
+  }
 }
