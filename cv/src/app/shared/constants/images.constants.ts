c392b17afd8f7878f2c765c8f72430f7d5700c47
Rolevich Maxim <79806004+Massonnka@users.noreply.github.com>
2021-08-09 11:17:04 +0300

    add constants images

---
+++ cv/src/app/shared/constants/images.constants.ts
@@ -0,0 +1,7 @@
+export const headLogo: string = '../assets/images/logo.png';
+
+export const logoAuthBlack: string = '../assets/images/logoAuthBlack.png';
+
+export const logoAuthWhite: string = '../assets/images/logoAuthWhite.png';
+
+export const bg: string = '../assets/images/bg.png';
