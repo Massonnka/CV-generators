0564c769e05bcebc3a8ea7458627b1b29b7f075d
Nikita Shcherbich <shcherbich.n@mail.ru>
2021-08-19 14:38:28 +0300

    fix styles

--- cv/src/app/modules/layout/pages/employee/employee.module.ts
+++ cv/src/app/modules/layout/pages/employee/employee.module.ts
@@ -6,15 +6,17 @@ import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzTableModule } from 'ng-zorro-antd/table';
-import { NgxPaginationModule, PaginatePipe } from 'ngx-pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { BreadcrumbModule } from 'src/app/shared/controls/breadcrumb/breadcrumb.module';
 import {
   breadcrumbReducer,
   breadcrumbsFeatureKey,
 } from 'src/app/shared/controls/breadcrumb/store/breadcrumbs.reducer';
 import { PrimaryButtonModule } from 'src/app/shared/controls/buttons/primary-button/primary-button.module';
+import { InputModule } from 'src/app/shared/controls/inputs/input/input.module';
 import { SpinnerModule } from 'src/app/shared/controls/spinner/spinner.module';
 import { TableModule } from 'src/app/shared/controls/table/table.module';
 import { SearchEmployeePipe } from 'src/app/shared/pipes/searchEmployee.pipe';
@@ -27,8 +29,6 @@ import { EmployeeAddProfileModule } from './employee-add-profile/employee-add-pr
 import { EmployeeInfoProfileModule } from './employee-info-profile/employee-info-profile.module';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { InputModule } from 'src/app/shared/controls/inputs/input/input.module';
 
 @NgModule({
   declarations: [EmployeeComponent, SearchEmployeePipe],
@@ -52,7 +52,6 @@ import { InputModule } from 'src/app/shared/controls/inputs/input/input.module';
     NzIconModule,
     SpinnerModule,
     NgxPaginationModule,
-    PaginatePipe,
     StoreModule.forFeature(
       breadcrumbsFeatureKey || employeeFeatureKey,
       breadcrumbReducer || employeeReducer
